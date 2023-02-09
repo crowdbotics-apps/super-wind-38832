876d3915212f21c0a95e7ba678796165d5ec1f19
Crowdbotics <team@crowdbotics.com>
2023-02-09 17:47:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-wind-38832",
+  url: "https://super-wind-38832.botics.co",
+  api: "https://super-wind-38832.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
